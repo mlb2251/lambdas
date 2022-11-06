54345974c1232f9d8d0ec30300ffcb928f4dc6b0
Matthew Bowers <mlbowers@mit.edu>
2022-11-05 21:20:38 -0400

    parsing of alt_exprs works

--- src/alt_expr.rs
+++ src/alt_expr.rs
@@ -1,10 +1,12 @@
+use core::num;
 // use crate::*;
 // use crate::parse_expr::{curry_sexp,uncurry_sexp};
 use std::collections::HashMap;
 use std::fmt::{self, Formatter, Display, Debug};
 use std::hash::Hash;
-use std::ops::{Index, IndexMut};
+use std::ops::{Index, IndexMut, Range};
 use serde::{Serialize, Deserialize};
+use std::cmp::{min,max};
 
 
 pub type Idx = usize;
@@ -61,13 +63,15 @@ pub enum Node where
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ExprSet {
     pub nodes: Vec<Node>,
+    pub subtree_spans: Option<Vec<Range<Idx>>>,
     pub order: Order
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
 pub enum Order {
     ChildFirst,
-    ParentFirst
+    ParentFirst,
+    Any
 }
 
 #[derive(Clone,Copy)]
@@ -75,7 +79,6 @@ pub struct Expr<'a> {
     set: &'a ExprSet,
     idx: Idx 
 }
-
 
 pub struct ExprMut<'a> {
     set: &'a mut ExprSet,
@@ -95,14 +98,37 @@ impl IndexMut<Idx> for ExprSet {
         &mut self.nodes[idx]
     }
 }
+impl Index<Range<Idx>> for ExprSet {
+    type Output = [Node];
+    #[inline(always)]
+    fn index(&self, idx: Range<Idx>) -> &Self::Output {
+        &self.nodes[idx]
+    }
+}
+impl IndexMut<Range<Idx>> for ExprSet {
+    #[inline(always)]
+    fn index_mut(&mut self, idx: Range<Idx>) -> &mut Self::Output {
+        &mut self.nodes[idx]
+    }
+}
 
 impl ExprSet {
-    fn empty(order: Order) -> ExprSet {
-        ExprSet { nodes: vec![], order }
+    fn empty(order: Order, subtree_spans: bool) -> ExprSet {
+        let subtree_spans = if subtree_spans { Some(vec![]) } else { None };
+        ExprSet { nodes: vec![], order, subtree_spans }
     }
     fn add(&mut self, node: Node) -> Idx {
+        let idx = self.nodes.len();
+        if let Some(spans) = &mut self.subtree_spans {
+            let span = match node {
+                Node::Var(_) | Node::Prim(_) | Node::IVar(_) => idx .. idx+1,
+                Node::App(f, x) => min(min(spans[f].start,spans[x].start),idx) .. max(max(spans[f].end,spans[x].end),idx+1),
+                Node::Lam(b) => min(spans[b].start,idx) .. max(spans[b].end,idx+1)
+            };
+            spans.push(span);
+        }
         self.nodes.push(node);
-        self.nodes.len() - 1 
+        idx
     }
     fn get(&self, idx: Idx) -> Expr {
         Expr { set: self, idx }
@@ -169,5 +195,195 @@ impl<'a> Display for Expr<'a> {
             }
         }
         fmt_local(*self, false, f)
+    }
+}
+
+
+impl ExprSet {
+    fn parse_extend(&mut self, s_init: &str) -> Result<Idx,String> {
+        let init_len = self.nodes.len();
+
+        let mut s = s_init.trim();
+
+        let mut items: Vec<Idx> = vec![];
+        let mut items_of_depth: Vec<usize> = vec![]; // offsets[i] gives the number of items at depth i
+        items_of_depth.push(0); // the zero paren depth
+
+        while !s.trim().is_empty() {
+            s = s.trim();
+            let next =  s.chars().last().unwrap();
+            if next == '(' {
+                s = &s[..s.len()-1];
+                let num_items = items_of_depth.pop().ok_or_else(||format!("ExprSet parse error: mismatched parens in: {}",s_init))?;
+                if num_items == 0 {
+                    continue
+                }
+
+                
+                // now num_items >= 1. The following loop will only happen if num_items >= 2.
+                // apply the last item to the second to last, etc
+                for _ in 0..num_items-1 {
+                    // println!("built an app inside");
+                    let f: Idx = items.pop().unwrap();
+                    let x: Idx = items.pop().unwrap();
+                    items.push(self.add(Node::App(f, x)))
+                }
+                // then we simply leave that final result pushed on
+                if let Some(num_items) = items_of_depth.last_mut() {
+                    *num_items += 1;
+                } else {
+                    return Err(format!("ExprSet parse error: mismatched parens in: {}",s_init));
+                }
+                continue
+            }
+            if next == ')' {
+                s = &s[..s.len()-1];
+                items_of_depth.push(0);
+                continue
+            }
+            // parse a space-separated word
+            // println!("parsing with s: `{}`", s);
+            let start = {
+                let mut i = s.len()-1;
+                loop {
+                    if i == 0 {
+                        // println!("break at i==0");
+                        break
+                    }
+                    let c = s.chars().nth(i-1).unwrap();
+                    if c.is_whitespace() || c == '(' || c == ')' {
+                        // println!("break at c: {}", c);
+                        break
+                    }
+                    i -= 1;
+                }
+                // println!("i: {}", i);
+                i
+            };
+            let item_str = &s[start..];
+            // println!("item_str: {}", item_str);
+            s = &s[..start];
+
+            if item_str == "lam" {
+                // println!("remainder: {}",s);
+                let mut eof = false;
+                if let Some(c) = s.chars().last()  {
+                    if c != '(' {
+                        return Err(format!("ExprSet parse error: `lam` must always have an immediately preceding parenthesis like so `(lam` unless its at the start of the parsed string: {}",s_init))
+                    }
+                    s = &s[..s.len()-1]; // strip "("
+                } else {
+                    eof = true;
+                };
+
+                let num_items = items_of_depth.pop().ok_or_else(||format!("ExprSet parse error: mismatched parens in: {}",s_init))?;
+                if num_items != 1 {
+                    return Err(format!("ExprSet parse error: `lam` must always be applied to exactly one argument, like `(lam (foo bar))`: {}",s_init))
+                }
+                let b: Idx = items.pop().unwrap();
+                items.push(self.add(Node::Lam(b)));
+                // println!("added lam");
+                if eof {
+                    if items.len() != 1 {
+                        return Err(format!("ExprSet parse error: mismatched parens in: {}",s_init));
+                    }
+                    return Ok(items.pop().unwrap())
+                }
+                if let Some(num_items) = items_of_depth.last_mut() {
+                    *num_items += 1;
+                } else {
+                    return Err(format!("ExprSet parse error: mismatched parens in: {}",s_init));
+                }
+                continue
+            }
+
+            let node = {
+                if let Some(rest) = item_str.strip_prefix("$") {
+                    Node::Var(rest.parse::<i32>().map_err(|e|e.to_string())?)
+                } else if let Some(rest) = item_str.strip_prefix("#") {
+                    Node::IVar(rest.parse::<i32>().map_err(|e|e.to_string())?)
+                } else {
+                    Node::Prim(item_str.into())
+                }
+            };
+            items.push(self.add(node));
+            *items_of_depth.last_mut().unwrap() += 1;
+        }
+
+        if items.len() == 0 {
+            return Err("ExprSet parse error: input is empty string".to_string());
+        }
+
+        if items_of_depth.len() != 1 {
+            return Err(format!("ExprSet parse error: mismatched parens in: {}",s_init));
+        }
+
+        let num_items = items_of_depth.pop().unwrap();
+        // println!("items outside: {}", num_items);
+        for _ in 0..num_items-1 {
+            // println!("built an app outside");
+            let f: Idx = items.pop().unwrap();
+            let x: Idx = items.pop().unwrap();
+            items.push(self.add(Node::App(f, x)))
+        }
+        if items.len() != 1 {
+            return Err(format!("ExprSet parse error: mismatched parens in: {}",s_init));
+        }
+
+        if self.order == Order::ParentFirst {
+            self.nodes[init_len..].reverse();
+        }
+        Ok((items.pop().unwrap()))
+    }
+}
+
+impl std::str::FromStr for ExprSet {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // assume uncurried string
+        let mut set = ExprSet::empty(Order::ChildFirst, false);
+        set.parse_extend(s)?;
+        Ok(set)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn assert_parse(set: &mut ExprSet, in_s: &str, out_s: &str) {
+        let idx = set.parse_extend(in_s).unwrap();
+        assert_eq!(set.get(idx).to_string(), out_s.to_string());
+    }
+
+    #[test]
+    fn test_parse() {
+        let set = &mut ExprSet::empty(Order::ChildFirst, false);
+        assert_parse(set, "(+ 2 3)", "(+ 2 3)");
+        assert_parse(set, "(+ 2 3)", "(+ 2 3)");
+
+        assert_parse(set, "3", "3");
+        assert_parse(set, "foo", "foo");
+
+        assert_parse(set, "(foo (bar baz))", "(foo (bar baz))");
+        assert_parse(set, "((foo bar) baz)", "(foo bar baz)");
+
+        assert_parse(set, "foo bar baz", "(foo bar baz)");
+
+        assert_parse(set, "(lam b)", "(lam b)");
+
+        assert_parse(set, "lam b", "(lam b)");
+        assert_parse(set, "(foo (lam b) (lam c))", "(foo (lam b) (lam c))");
+        assert_parse(set, "(lam (+ a b))", "(lam (+ a b))");
+        assert_parse(set, "(lam (+ $0 b))", "(lam (+ $0 b))");
+        assert_parse(set, "(lam (+ #0 b))", "(lam (+ #0 b))");
+
+        let idx = set.parse_extend("$3").unwrap();
+        assert_eq!(set.get(idx).node(), &Node::Var(3));
+        let idx = set.parse_extend("#3").unwrap();
+        assert_eq!(set.get(idx).node(), &Node::IVar(3));
+
+        assert_parse(set, "(fix_flip $0 (lam (lam (if (is_empty $0) $0 (cons (+ 1 (head $0)) ($1 (tail $0)))))))", "(fix_flip $0 (lam (lam (if (is_empty $0) $0 (cons (+ 1 (head $0)) ($1 (tail $0)))))))")
+
     }
 }
